80c280ea610f94d2cf6a4adf2f4fa2d5d8391042
Francis Rainier Cutamora <169155198+Iniw41@users.noreply.github.com>
2025-05-18 03:00:25 +0800

    Bro this is actual  Progress

---
+++ Chat_Box/NVVM/View Model/MainViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat_Box.Core;
+using Chat_Box.NVVM.Model;
+
+namespace Chat_Box.NVVM.View_Model
+{
+    class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+        public  RelayCommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set 
+            {
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { 
+                _message = value; 
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Iniw",
+                    Username_Color = "#409AFF",
+                    Image_Source = "Icons/Biniw.png",
+                    Message = Message,
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = true,
+                });
+                Message = "";
+            });
+
+            Messages.Add(new MessageModel()
+            {
+                Username = "John Doe",
+                Username_Color = "#409AFF",
+                Image_Source = "Icons/Biniw.png",
+                Message = "Nigga",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel()
+                {
+                    Username = "Alo",
+                    Username_Color = "#409AFF",
+                    Image_Source = "Icons/Biniw.png",
+                    Message = "Nigga",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel()
+                {
+                    Username = "John Doe",
+                    Username_Color = "#409AFF",
+                    Image_Source = "Icons/Biniw.png",
+                    Message = "Nigga",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true,
+                });
+            }
+
+            Messages.Add(new MessageModel()
+            {
+                Username = "John Doe",
+                Username_Color = "#409AFF",
+                Image_Source = "Icons/Biniw.png",
+                Message = "Bitch Ass",
+                Time = DateTime.Now,
+                IsNativeOrigin = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Biniw {i}",
+                    Image_Source = "Icons/Biniw.png",
+                    Messages = Messages
+                });
+            }
+        }
+    }
+}
